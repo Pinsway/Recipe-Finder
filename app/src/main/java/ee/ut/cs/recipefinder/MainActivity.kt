ff13699bf1fedf701fd8d42d910311ac560042f3
erikanmann <erikanmann@gmail.com>
2025-10-29 00:32:09 +0200

    added temporary test for working API call (JSON displayed on screen)

--- app/src/main/java/ee/ut/cs/recipefinder/MainActivity.kt
+++ app/src/main/java/ee/ut/cs/recipefinder/MainActivity.kt
@@ -10,7 +10,12 @@ import androidx.activity.compose.setContent
 // import ee.ut.cs.recipefinder.domain.model.Recipe
 import ee.ut.cs.recipefinder.ui.navigation.AppNavigation
 import ee.ut.cs.recipefinder.ui.theme.RecipeFinderTheme
- 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.lifecycle.lifecycleScope
+import com.google.gson.Gson
+import kotlinx.coroutines.launch
+import ee.ut.cs.recipefinder.data.remote.MealDbRetrofit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +26,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             RecipeFinderTheme {
                 AppNavigation()
+
+
+                // temporary test for working API call (JSON displayed on screen)
+                // var jsonText by remember { mutableStateOf("Loading...") }
+
+                // Launch API call when the screen opens
+                // LaunchedEffect(Unit) {
+                //    try {
+                //        val response = MealDbRetrofit.api.searchMeals("chicken")
+                //        jsonText = Gson().toJson(response)
+                //    } catch (e: Exception) {
+                //        jsonText = "Error: ${e.message}"
+                //    }
+                //}
+
+                // Display the JSON or error message
+                // Text(text = jsonText)
             }
         }
     }
