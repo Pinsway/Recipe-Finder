aff8c9a2873fb187beb31d0200b0dec319109df2
oskarpro <oskar.proos@ut.ee>
2025-12-18 18:57:52 +0200

    Changed font for better visibility

--- app/src/main/java/ee/ut/cs/recipefinder/ui/theme/Type.kt
+++ app/src/main/java/ee/ut/cs/recipefinder/ui/theme/Type.kt
@@ -8,11 +8,14 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp
+    ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 24.sp
     )
 )
